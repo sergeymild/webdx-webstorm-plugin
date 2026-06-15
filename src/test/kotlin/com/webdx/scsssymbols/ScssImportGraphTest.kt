20bc34259f0aeb11f049f5541b2562e412f83825
SergeyMild <>
2026-06-15 08:37:43 +0300

    test(scsssymbols): assert a real unrelated file is absent from provided()

--- src/test/kotlin/com/webdx/scsssymbols/ScssImportGraphTest.kt
+++ src/test/kotlin/com/webdx/scsssymbols/ScssImportGraphTest.kt
@@ -5,16 +5,18 @@ import com.intellij.testFramework.fixtures.BasePlatformTestCase
 class ScssImportGraphTest : BasePlatformTestCase() {
 
     fun testProvidedFollowsForwardAndImportTransitively() {
-        val c = myFixture.addFileToProject("c.scss", "\$c: 1;")
-        val b = myFixture.addFileToProject("b.scss", "@forward './c.scss';\n\$b: 1;")
+        myFixture.addFileToProject("c.scss", "\$c: 1;")
+        myFixture.addFileToProject("b.scss", "@forward './c.scss';\n\$b: 1;")
         val a = myFixture.addFileToProject("a.scss", "@forward './b.scss';\n\$a: 1;")
+        // a real, unrelated file that `a` neither forwards nor imports
+        val unrelated = myFixture.addFileToProject("unrelated.scss", "\$u: 1;")
         val provided = ScssImportGraph.provided(project, a.virtualFile)
         assertEquals(
             setOf("a.scss", "b.scss", "c.scss"),
             provided.map { it.name }.toSet(),
         )
-        // unrelated file not pulled in
-        assertFalse(provided.contains(c.virtualFile.parent.findChild("nope.scss")))
+        // unrelated existing file is not pulled in
+        assertFalse(provided.contains(unrelated.virtualFile))
     }
 
     fun testProvidedIsCycleSafe() {
